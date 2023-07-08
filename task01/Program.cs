a1391bc621f0453a19289b8dc29986474c5a148d
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-07-08 14:28:43 +0300

    Added second task

---
+++ task01/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 4: Напишите программу,
+// которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите число 1: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число 2: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число 3: ");
+int thirdNumber = int.Parse(Console.ReadLine()!);
+
+int maximum = firstNumber;
+
+if (secondNumber > maximum)
+    maximum = secondNumber;
+if (thirdNumber > maximum)
+    maximum = thirdNumber;
+
+Console.WriteLine("Максимальное число: " + maximum);
